df0f229954cc7e25678394321b0d20b57814b803
Kamil Tonkowicz <kamil.tonkowicz@gmail.com>
2026-07-11 18:49:54 +0200

    chore: ignore eslint and ts errors during build

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
     "csv-parse",
     "jsonwebtoken",
   ],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
